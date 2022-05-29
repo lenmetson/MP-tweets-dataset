3d81ba446ff4107bd916f6e076ca746bf1c86ef4
Len Metson <lennardmetson@gmail.com>
2022-05-29 22:33:23 +0100

    Create pull-latest-MPs.R

---
+++ code/pull-latest-MPs.R
@@ -0,0 +1,12 @@
+# Script to pull latest MPs from www.politics-social.com
+
+if(!require("here"))install.packages("here")
+source(here("code", "packages.R") # CREATE THIS FILE
+
+url <- "https://www.politics-social.com/api/list/csv/name"
+download.file(url, here("input_data", "MPs_Twitter.csv"))
+
+url2 <- "https://www.politics-social.com/api/list/csv/unlisted"
+download.file(url2, here("input_data", "MPs_noTwitter.csv"))
+
+rm(url, url2)
