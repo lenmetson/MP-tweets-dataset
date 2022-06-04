f67f1568703d9fc47bf5bc507709a14dbd340f2b
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-04 12:35:29 +0100

    Create MVP of id pulls
    I want to create a loop that does this regardless of the number of rows in the original dataset.
    
    Also should investigate why some are missing

--- code/2_extract-twitter-ids.R
+++ code/2_extract-twitter-ids.R
@@ -1,1 +1,59 @@
 # Code to take user names and return twitter IDs to then pull tweets
+
+MPs_Twitter <- read.csv(here("input_data", "MPs_Twitter.csv"))
+
+screen_names <- c(MPs_Twitter$Screen.name) %>%
+  str_replace("@", "") # We need to remove the @ symbol 
+
+screen_names_split <- split(screen_names, ceiling(seq_along(screen_names)/100)) # split names into batches of 100 (as this is how many the API will take per call)
+
+
+ceiling(nrow(MPs_Twitter)/100) # way of working out how many batches there will be
+
+
+user_ids1 <- get_user_id(
+  screen_names_split$`1`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+user_ids2 <- get_user_id(
+  screen_names_split$`2`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+user_ids3 <- get_user_id(
+  screen_names_split$`3`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+user_ids4 <- get_user_id(
+  screen_names_split$`4`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+user_ids5 <- get_user_id(
+  screen_names_split$`5`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+user_ids6 <- get_user_id(
+  screen_names_split$`6`, 
+  bearer_token = get_bearer(),
+  all = TRUE, 
+  keep_na = TRUE
+)
+
+
+# Bind user_ids 
+
+user_ids <- rbind(user_ids1, user_ids3, user_ids3, user_ids4, user_ids5, user_ids6)
