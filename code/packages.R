34f1b5746bee505b1573ae970201c462d4b55b22
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-04 11:02:57 +0100

    Add packages script

--- code/packages.R
+++ code/packages.R
@@ -1,1 +1,13 @@
+# Run this script to open libraries
+
+need <- c(
+          "tidyverse")
+
+have <- need %in% rownames(installed.packages()) # checks packages you have
+
+if(any(!have)) install.packages(need[!have]) # install missing packages
+
+invisible(lapply(need, library, character.only=T)) # load needed packages
+
+rm(have, need) # remove objects
 
