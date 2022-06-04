c2babc4f8c27b8c5cdef3d937358931ef5433b0c
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-04 11:02:45 +0100

    Delete old datasets

--- code/1_pull-latest-MPs.R
+++ code/1_pull-latest-MPs.R
@@ -1,7 +1,7 @@
 # Script to pull latest MPs from www.politics-social.com
 
 if(!require("here"))install.packages("here")
-source(here("code", "packages.R") # CREATE THIS FILE
+source(here("code", "packages.R")) # CREATE THIS FILE
 
 url <- "https://www.politics-social.com/api/list/csv/name"
 download.file(url, here("input_data", "MPs_Twitter.csv"))
