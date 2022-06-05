5902a76c81745a951103da68701e48f1faadc891
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-05 18:30:30 +0100

    Update some comments

--- code/2_extract-twitter-ids.R
+++ code/2_extract-twitter-ids.R
@@ -5,62 +5,22 @@ MPs_Twitter <- read.csv(here("input_data", "MPs_Twitter.csv"))
 screen_names <- c(MPs_Twitter$Screen.name) %>%
   str_replace("@", "") # We need to remove the @ symbol 
 
+
+
+# We can't call all of the MPs at the same time, so we need to split them into batches then create separate functions 
+
 screen_names_split <- split(screen_names, ceiling(seq_along(screen_names)/100)) # split names into batches of 100 (as this is how many the API will take per call)
 
 
 # Function that writes the batches we need to run 
 fun1 <- function(r) {
-  fun1x <- paste0("user_ids", r, " <- get_user_id(", "screen_names_split$`", r, "`, bearer_token = get_bearer(), all = TRUE, keep_na = TRUE)" )
-  return(fun1x)
+  fun1_output <- paste0("user_ids", r, " <- get_user_id(", "screen_names_split$`", r, "`, bearer_token = get_bearer(), all = TRUE, keep_na = TRUE)" )
+  return(fun1_output)
 }
 
 p2 <- fun1(1:ceiling(nrow(MPs_Twitter)/100))
 
-
-
-
-
-user_ids1 <- get_user_id(
-  screen_names_split$`1`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
-
-user_ids2 <- get_user_id(
-  screen_names_split$`2`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
-
-user_ids3 <- get_user_id(
-  screen_names_split$`3`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
-
-user_ids4 <- get_user_id(
-  screen_names_split$`4`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
-
-user_ids5 <- get_user_id(
-  screen_names_split$`5`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
-
-user_ids6 <- get_user_id(
-  screen_names_split$`6`, 
-  bearer_token = get_bearer(),
-  all = TRUE, 
-  keep_na = TRUE
-)
+# Need to find a way to sequentially run output of p2 
 
 
 # Bind user_ids 
