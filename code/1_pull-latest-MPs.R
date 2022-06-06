322b2ceb4b3cc39fa84b79519af084682feb19a4
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-06 16:03:24 +0100

    Put date back in then assign within script 1

--- code/1_pull-latest-MPs.R
+++ code/1_pull-latest-MPs.R
@@ -1,9 +1,13 @@
 # Script to pull latest MPs from www.politics-social.com
 
-url <- "https://www.politics-social.com/api/list/csv/name"
-download.file(url, here("input_data", "MPs_Twitter.csv"))
+url1 <- "https://www.politics-social.com/api/list/csv/name"
+eval(parse(text = paste0("download.file(url1, here('input_data', 'MPs_Twitter_", Sys.Date(), ".csv'))")))
 
 url2 <- "https://www.politics-social.com/api/list/csv/unlisted"
-download.file(url2, here('input_data', 'MPs_noTwitter.csv'))
+eval(parse(text = paste0("download.file(url2, here('input_data', 'MPs_noTwitter_", Sys.Date(), ".csv'))")))
 
 rm(url, url2)
+
+
+eval(parse(text = paste0("MPs_Twitter <- read.csv(here('input_data', 'MPs_Twitter_", Sys.Date(), ".csv'))")))
+eval(parse(text = paste0("MPs_noTwitter <- read.csv(here('input_data', 'MPs_noTwitter_", Sys.Date(), ".csv'))")))
