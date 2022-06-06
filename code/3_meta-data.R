8992541fa3d559c1ce563d55d63bf78946b394f1
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-06 16:03:09 +0100

    Update 3_meta-data.R

--- code/3_meta-data.R
+++
@@ -1,7 +0,0 @@
-library(WikidataR)
-
-eval(parse(text = paste0("MPs <- read_rds(here('input_data', 'MPs_Twitter_ids_", Sys.Date(), ".rds'))"))) 
-
-names <- MPs$Name
-
-wiki_ids <- qid_from_name(name = names, limit = 100, format = "vector")
