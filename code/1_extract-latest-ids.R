1f5072f1654291986ee4b819ac27f834f53378da
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-06-07 13:07:30 +0100

    Combine scripts 1 and 2

---
+++ code/1_extract-latest-ids.R
@@ -0,0 +1,75 @@
+# Pull latest MPs from www.politics-social.com
+
+url1 <- "https://www.politics-social.com/api/list/csv/name"
+eval(parse(text = paste0("download.file(url1, here('input_data', 'MPs_Twitter_", Sys.Date(), ".csv'))")))
+
+url2 <- "https://www.politics-social.com/api/list/csv/unlisted"
+eval(parse(text = paste0("download.file(url2, here('input_data', 'MPs_noTwitter_", Sys.Date(), ".csv'))")))
+
+rm(url1, url2)
+
+
+eval(parse(text = paste0("MPs_Twitter <- read.csv(here('input_data', 'MPs_Twitter_", Sys.Date(), ".csv'))")))
+eval(parse(text = paste0("MPs_noTwitter <- read.csv(here('input_data', 'MPs_noTwitter_", Sys.Date(), ".csv'))")))
+
+# Code to take user names and return twitter IDs to then pull tweets
+
+
+screen_names <- c(MPs_Twitter$Screen.name) %>%
+  str_replace("@", "") # We need to remove the @ symbol
+
+
+
+# We can't call all of the MPs at the same time, so we need to split them into batches then create separate functions
+
+screen_names_split <- split(screen_names, ceiling(seq_along(screen_names)/50)) # split names into batches of 50 (as there is a limit of how many the API will take per call)
+
+
+# Write loop that outputs batches of user ids
+
+for (x in 1:ceiling(nrow(MPs_Twitter)/50)) {
+  eval(
+    parse(
+      text =
+        paste0("user_ids",
+               x,
+               " <- get_user_id(screen_names_split$`",
+               x, "`, bearer_token = get_bearer(), all = TRUE, keep_na = TRUE)")
+      )
+    )
+}
+
+
+
+fn_bind <- function(i) {
+  fn_dfs <- paste0("user_ids", i)
+  return(rbind(fn_dfs))
+
+}
+
+dfs <- fn_bind(1:ceiling(nrow(MPs_Twitter)/50))
+dfs <- paste(dfs, collapse = ",")
+
+eval(parse(text = paste0("user_ids <- rbind(", dfs, ")")))
+
+
+# Bind back into MPs_Twitter
+
+user_ids$Screen.name <- paste0("@", user_ids$username) # add back @ symbol
+
+user_ids <- user_ids %>% rename(
+  "twiter_name_at_pull" = "name",
+  "username_for_pull" = "username"
+)
+
+MPs_Twitter_with_ids <- left_join(MPs_Twitter, user_ids, by = "Screen.name")
+
+
+missing_ids <- MPs_Twitter_with_ids %>% filter(is.na(id)) # save MPs whos ID is missing and perform necessary manual validation as to why
+
+# Write out final files
+
+eval(parse(text = paste0("write_rds(MPs_Twitter_with_ids, here('input_data', 'MPs_Twitter_ids_", Sys.Date(), ".rds'))")))
+eval(parse(text = paste0("write_rds(missing_ids, here('input_data', 'missing_ids_", Sys.Date(), ".rds'))")))
+
+rm(list=ls()[! ls() %in% c("MPs_Twitter_with_ids", "missing_ids")])
--- code/3_meta-data.R
+++ code/3_meta-data.R
@@ -1,5 +1,6 @@
 wiki_data <- read.csv(here('input_data', 'wiki_query.csv'))
 
+
 MPs_Twitter_with_ids$name_merge <- gsub(" ", "", MPs_Twitter_with_ids$Name, fixed = TRUE)  
 MPs_Twitter_with_ids$name_merge <- MPs_Twitter_with_ids$name_merge %>% str_trim() %>% tolower()
 
